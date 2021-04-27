d25142a4012cd45809177a2681931e87731bb88b
Maxim Koylo <lkoylol12@gmail.com>
2021-04-27 08:57:19 +0300

    update readme

--- src/constants/timeouts.ts
+++ src/constants/timeouts.ts
@@ -1,5 +1,5 @@
-export const BLOCK_SEND_ROULETTE = 30 * 60 * 1000;
-export const BLOCK_SEND_CASINO = 30 * 60 * 1000;
+export const BLOCK_SEND_ROULETTE = 1 * 60 * 60 * 1000;
+export const BLOCK_SEND_CASINO = 2 * 60 * 60 * 1000;
 export const BLOCK_SEND_RATING = 3 * 60 * 1000;
 
 export const WAIT_SEND_BET_CASINO = 1 * 60 * 1000;
